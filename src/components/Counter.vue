de3b7a105160904830c96a4652921620335bb801
Cristian Sulbaran <csulbaran@sugit.com.ar>
2021-11-25 23:36:13 -0300

    Primer componente básico creado

---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Counter: 10</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter", //Facilita la visualización, por lo menos en el Vue Dev tools
+};
+</script>
+
+<style>
+</style>
